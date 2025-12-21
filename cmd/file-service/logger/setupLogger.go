2cd9d52fdd363f08c3fc6d34510a92c4ba710fd3
Grigoriy <dinsuil275283@gmail.com>
2025-12-21 17:27:02 +0500

    Logger setup

---
+++ cmd/file-service/logger/setupLogger.go
@@ -0,0 +1,40 @@
+package logger
+
+import (
+	"file-service/pkg/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = setupPrettySlog()
+	case envProd:
+		log = setupPrettySlog()
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
